50b877c4840396c77af395bd738acebde43ac930
MINIVISION\zhoumingye <zhoumingye@minivision.cn>
2019-04-23 10:51:52 +0800

    质量分析页面完毕

--- src/components/operation-tools/query_quality.vue
+++ src/components/operation-tools/query_quality.vue
@@ -14,45 +14,37 @@
                 start-placeholder="开始日期"
                 end-placeholder="结束日期"
                 v-model="queryParams.time"
+                :name='["start", "end"]'
                 range-separator="至"
                 :picker-options="pickerOptions2">
               </el-date-picker>
             </div>
           </el-form-item>
-          <el-form-item label="行业类型：" prop="type">
-            <el-select @change="changeType" v-model="queryParams.type" placeholder="请选择">
-              <el-option
-                v-for="v in businessType"
-                :key="v.typeId"
-                :label="v.typeName"
-                :value="v.typeId">
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="客户名称：" prop="loginName">
-            <el-select filterable v-model="queryParams.loginName" placeholder="请选择">
-              <el-option
-                v-for="v in loginName"
-                @click.native.stop="changeCustomer(v)"
-                :key="v.customerId"
-                :title="`${v.customerName}(${v.loginName})`"
-                :data-customerid="v.customerId"
-                :label="v.customerName"
-                :value="v.loginName">
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="接口类型：" prop="serviceName">
-            <el-select filterable v-model="queryParams.serviceName" placeholder="请选择">
-              <el-option
-                v-for="v in services"
-                :key="v.serviceId"
-                :title="`${v.serviceNameZh}(${v.serviceName})`"
-                :label="v.serviceNameZh"
-                :value="v.serviceName">
-              </el-option>
-            </el-select>
-          </el-form-item>
+          <Select 
+            :labelTitle="'行业类型'" 
+            :originArr="businessType" 
+            :defaultValue="'typeId'" 
+            :defaultLable="'typeName'"
+            :isAll = true
+            @changeInputValue="changeType"> 
+          </Select>
+          <loginNameSelect 
+            :labelTitle="'客户名称'" 
+            :originArr="loginName" 
+            :defaultValue="'loginName'" 
+            :defaultLable="'customerName'"
+            :needValue="'customerId'"
+            :searchInput=true
+            :isAll=true
+            @changeInputValue="changeCustomer">
+          </loginNameSelect>
+          <serviceSelect 
+            :labelTitle="'接口类型'" 
+            :originArr="services" 
+            :defaultValue="'serviceName'" 
+            :searchInput = true
+            :defaultLable="'serviceNameZh'">
+          </serviceSelect>
           <el-form-item class="query-item">
            <query-button @reset="reset" @submit="onSubmit"></query-button>
           </el-form-item>
@@ -70,8 +62,8 @@
       <div class="card-container">
         <div v-show="!tabFlag && !tableData.length" ref="nocharts" class="no-charts" style="height:400px;width:100%;"></div>
         <div v-show="!tabFlag && tableData.length" class="charts" ref="charts" style="height:400px;width:100%;"></div>
-        <Table class="table" :tableData="tableData" :tatalPage="tableData.length" v-show="tabFlag">
-          <el-table-column
+        <Table class="table" :tableData="tableData" :tatalPage="tableData.length" v-show="tabFlag" :columns="columns">
+          <!-- <el-table-column
             label="使用日期"
             sortable
             prop="dayTime">
@@ -90,7 +82,7 @@
             label="消费金额"
             sortable
             prop="downCost">
-          </el-table-column>
+          </el-table-column> -->
         </Table>
       </div>
     </div>
@@ -104,37 +96,73 @@ import echarts from 'echarts'
 import { switchMixin, hotKeyTime, businessType, loginName, services } from '../../common/js/mixin'
 import Table from '../../base/Table'
 import QueryButton from '../../base/QueryButton'
+import loginNameSelect from '../../base/Select'
+import serviceSelect from '../../base/Select'
+import Select from '../../base/Select'
 export default {
   mixins: [switchMixin, hotKeyTime, businessType, loginName, services],
   data () {
     return {
       queryParams: {
-        time: [new Date().getTime() - 3600 * 1000 * 24 * 7, new Date()],/**默认时间最近七天 */
-        loginName: '',
-        serviceName: '',
-        type: ''
+        time: [new Date().getTime() - 3600 * 1000 * 24 * 7, new Date()]/**默认时间最近七天 */
       },
+      columns: [{ // 定义table
+        prop: 'serviceName',
+        minWidth: "160px",
+        label: '服务名'
+      }, { 
+        prop: 'serviceNameZh',
+        minWidth: "160px",
+        label: '服务名(中文)'
+      }, { 
+        prop: 'result',
+        label: 'RESULT'
+      }, { 
+        prop: 'resultCode',
+        label: 'resultCode'
+      }, { 
+        prop: 'usedCount',
+        label: '共计使用量'
+      }, { 
+        prop: 'downChargedCount',
+        label: '计费使用量'
+      }, { 
+        prop: 'downCost',
+        label: '消费金额',
+        formatter: row => {
+          return row.downCost.toFixed(4)
+        }
+      }],
       tableData: []
     }
   },
   components: {
     Table,
-    QueryButton
+    Select,
+    QueryButton,
+    serviceSelect,
+    loginNameSelect
   },
   methods: {
     reset () {
       this.$refs.querForm.resetFields()
-      this.queryParams.serviceName = this.services[0].serviceName
     },
     onSubmit () {
-      this.$refs.querForm.validate((valid) => {
-        if (valid) {
-         this.UsageByResultNew()
+      let options = {}
+      this.$refs.querForm.$el.querySelectorAll('input').forEach(v => {
+        if (v.name) {
+          if (v.name === 'typeId') {
+            options.businessType = v.value
+          } else {
+            options[v.name] = v.value
+          }
         }
       })
+      delete options.typeId
+      this.UsageByResultNew(options)
     },
-    UsageByResultNew () {
-      $http(this.API.downApi.UsageByResultNew, this.queryParams).then((res) => {
+    UsageByResultNew (options) {
+      $http(this.API.downApi.UsageByResultNew, options).then((res) => {
         let xAxisData = [], 
           series= [{
             name: '共计使用量',
